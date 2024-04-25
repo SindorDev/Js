487210b124ac4f54083d2c6e7c43c263f4381e23
Sindor007 <thesso003@gmail.com>
2024-04-25 10:53:50 +0500

    first commit

---
+++ home4/app.js
@@ -0,0 +1,3 @@
+const year = Number(prompt("Tugilgan yilingizni kiriting:"));
+const age = 2024 - year;
+document.write("siz ", + " " + age + " " + " " + "yoshdasiz")
---
+++ home5/app.js
@@ -0,0 +1,7 @@
+let height = Number(prompt("To'rtburchakning balanligini kiriting: "));
+let width = Number(prompt("To'rtburchakning uzunligini kiriting: "));
+
+let yigindi = height + width
+let result = yigindi * 4
+document.write(yigindi * 2 + " " )
+document.write(result)
